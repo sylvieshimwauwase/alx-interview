8d97055eac8232957351d8fc506eb6e530564cfb
sylvieshimwauwase <slvshimwa@gmail.com>
2024-05-10 17:07:34 +0200

    island parameter error debugging

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -2,20 +2,18 @@
 """Island perimeter module"""
 
 def island_perimeter(grid):
-      perimeter = 0
-      rows = len(grid)
-      cols = len(grid[0])
-
-      for i in range(rows):
-        for j in range(cols):
-            if grid[i][j] == 1:
-                if i == 0 or grid[i-1][j] == 0:
+    """A function that outputs the perimeter of an island described by grid"""
+    perimeter = 0
+    grid_length = len(grid)
+    for row in range(grid_length):
+        for column in range(len(grid[row])):
+            if grid[row][column] == 1:
+                if row - 1 < 0 or grid[row - 1][column] == 0:
                     perimeter += 1
-                if i == rows - 1 or grid[i+1][j] == 0:
+                if column - 1 < 0 or grid[row][column - 1] == 0:
                     perimeter += 1
-                if j == 0 or grid[i][j-1] == 0:
+                if column + 1 >= len(grid[row]) or grid[row][column + 1] == 0:
                     perimeter += 1
-                if j == cols - 1 or grid[i][j+1] == 0:
+                if row + 1 >= grid_length or grid[row + 1][column] == 0:
                     perimeter += 1
-
-      return perimeter
+    return perimeter
