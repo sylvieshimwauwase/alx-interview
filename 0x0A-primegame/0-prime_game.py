576ea16f69881da38b1d02f10cbab15097b07b7a
sylvieshimwauwase <slvshimwa@gmail.com>
2024-05-16 22:44:42 +0200

    performing prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,42 +2,54 @@
 """performing prime numbers game"""
 
 
-def isWinner(x, nums):
-    # Function to check if a number is prime
-    def is_prime(num):
-        if num < 2:
+def is_prime(n):
+    """ Checks if a number given n is a prime number """
+    for i in range(2, int(n ** 0.5) + 1):
+        if not n % i:
             return False
-        for i in range(2, int(num**0.5) + 1):
-            if num % i == 0:
-                return False
-        return True
+    return True
 
-    # Function to get primes up to n
-    def get_primes(n):
-        primes = []
-        for i in range(2, n + 1):
+
+def calculate_primes(n, primes):
+    """ Calculate all primes """
+    top_prime = primes[-1]
+    if n > top_prime:
+        for i in range(top_prime + 1, n + 1):
             if is_prime(i):
                 primes.append(i)
-        return primes
+            else:
+                primes.append(0)
 
-    # Function to simulate the game
-    def play_game(n):
-        primes = get_primes(n)
-        if len(primes) % 2 == 0:
-            return "Ben"  # If there are even number of primes, Ben wins
+
+def isWinner(x, nums):
+    """
+    x is the number of rounds and nums is an array of n
+    Return: name of the player that won the most rounds
+    If the winner cannot be determined, return None
+    You can assume n and x will not be larger than 10000
+    """
+
+    players_wins = {"Maria": 0, "Ben": 0}
+
+    primes = [0, 0, 2]
+
+    calculate_primes(max(nums), primes)
+
+    for round in range(x):
+        sum_options = sum((i != 0 and i <= nums[round])
+                          for i in primes[:nums[round] + 1])
+
+        if (sum_options % 2):
+            winner = "Maria"
         else:
-            return "Maria"  # If there are odd number of primes, Maria wins
+            winner = "Ben"
 
-    # Play each round and keep track of the winners
-    wins = {"Maria": 0, "Ben": 0}
-    for i in range(x):
-        winner = play_game(nums[i])
-        wins[winner] += 1
+        if winner:
+            players_wins[winner] += 1
 
-    # Determine the player with the most wins
-    if wins["Maria"] > wins["Ben"]:
+    if players_wins["Maria"] > players_wins["Ben"]:
         return "Maria"
-    elif wins["Maria"] < wins["Ben"]:
+    elif players_wins["Ben"] > players_wins["Maria"]:
         return "Ben"
-    else:
-        return None
+
+    return None
