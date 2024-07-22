0d7911e5a05ced7546befee567544d94ea923045
sylvieshimwauwase <slvshimwa@gmail.com>
2024-07-22 12:24:13 +0200

    working on star wars characters in alx-interview

--- 0x06-starwars_api/1st_attempt.js
+++ 0x06-starwars_api/1st_attempt.js
@@ -5,21 +5,22 @@ const request = require('request');
 const filmNum = process.argv[2] + '/';
 const filmURL = 'https://swapi-api.hbtn.io/api/films/';
 
-// Makes an API request to get film information
+// Makes an API request to get film Information
 request(filmURL + filmNum, async function (err, res, body) {
   if (err) return console.error(err);
 
-  // Parse the response body to get the list of character URLs
-  const charURLList = JSON.parse(body).characters;
+// Parse the response body to get the list of character URLs
+const charURLList = JSON.parse(body).characters;
 
-  // Iterare through the character URLs and fect character information
-  // Make a request to each character URL
+// Iterate through the character URLs and fetc character information
   for (const charURL of charURLList) {
     await new Promise(function (resolve, reject) {
+       // Make a request to each character URl
+
       request(charURL, function (err, res, body) {
         if (err) return console.error(err);
 
-        // Parse the charcter nformation and print the character's name Resolve the promise to indicate completion
+        // Parse the character information and print the character's name
         console.log(JSON.parse(body).name);
         resolve();
       });
